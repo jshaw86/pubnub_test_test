1e3b7c072f1033d3888127c94b0ea2f1438b147e
Jordan Shaw <jordan@pubnub.com>
2015-07-06 14:19:28 -0700

    removed vm, added requirements.txt
    Signed-off-by: Jordan Shaw <jordan@pubnub.com>

---
+++ tests/isender_test.py
@@ -0,0 +1,16 @@
+import sender
+import unittest
+
+class SenderTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+    def test_publish_subscribe_past(self):
+        self.assertEquals(True,False);
+
+    def test_subscribe_publish(self):
+        self.assertEquals(True,False);
+
+    def test_cont_subscribe_publish(self):
+        self.assertEquals(True,False);
